6923757182def47f26c76ada92c46a755cc7d92d
KobusBooyens <kobusb@runninghill.co.za>
2025-03-19 19:40:03 +0200

    Added resourced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from "./static-data/dummy-users";
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    UserComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'first-angular-app';
+
+  title = 'First Angular App';
+  users = DUMMY_USERS;
+  selectedUser = signal<User | undefined>(undefined);
+
+  onUserSelected(user: User) {
+    console.log(user);
+    this.selectedUser.set(user);
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, computed, EventEmitter, input, Output, output } from '@angular/core';
+import { DUMMY_USERS } from '../static-data/dummy-users';
+import { User } from '../models/user';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+  //inputs
+  user = input.required<User>();
+
+  //outputs
+  selectedUser = output<User | undefined>();
+
+  //computed
+  imagePath = computed(() => `images/users/${this.user().avatar}`);
+
+  //methods
+  onUserSelected() {
+    this.selectedUser.emit(this.user());
+  }
+}
